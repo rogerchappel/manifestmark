b9d3edd43f210182dafe2e56bab6add08d8c19ac
Roger Chappel <miscanalysis@gmail.com>
2026-05-19 06:37:27 +1000

    feat: export public api

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export { scan } from "./scan.js";
+export { renderResult } from "./format/index.js";
+export type {
+  DependencySummary,
+  IssueSeverity,
+  OutputFormat,
+  PackageManifest,
+  PackageSummary,
+  ScanIssue,
+  ScanOptions,
+  ScanResult,
+  ScriptSummary
+} from "./types.js";
